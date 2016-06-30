07aa99712d26ce0cc3e24ddd223348e274656ad1
Gregory Cooke <gcooke3@gatech.edu>
2016-06-30 10:01:31 -0500

    .gitignore is now working

--- funsets/funsets/src/main/scala/funsets/Main.scala
+++
@@ -1,7 +0,0 @@
-package funsets
-
-object Main extends App {
-  import FunSets._
-  println(contains(singletonSet(1), 1))
-  println(contains(union(singletonSet(1), singletonSet(3)), 3))
-}
--- project/src/main/scala/demo/hello.scala
+++
@@ -1,8 +0,0 @@
-package demo
-
-/**
- * Created by Gregory on 6/22/2016.
- */
-class hello {
-  def sayHello(name: String): ""
-}
